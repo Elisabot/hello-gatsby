7e82cb9294393a94f03e3719ead66d1447a56f05
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2021-01-04 15:48:39 -0800

    display random img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+export default function Home() {
+  return (
+    <div style={{ color: `purple`, fontSize: `72px` }}>
+      <h1>Hello Gatsby!</h1>
+      <img src="https://source.unsplash.com/random/400x200" alt="random photo from unsplash" />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+    )
+}
