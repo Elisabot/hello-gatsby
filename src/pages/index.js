a442a3706389c38921ccb274d6908e94f8c5dc14
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2021-01-04 16:28:30 -0800

    components and passing props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: `purple`, fontSize: `72px` }}>
-      <h1>Hello Gatsby!</h1>
+      <Header headerText={`Here's Gatsby`} />
       <img src="https://source.unsplash.com/random/400x200" alt="random photo from unsplash" />
     </div>
     )
