19ae94c282e34cbd9d518221076c24dbf9922a90
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2021-01-04 17:12:08 -0800

    routing be like

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
-import React from "react"
-import Header from "../components/header"
+import React from 'react'
+import { Link } from 'gatsby'
+import Header from '../components/header'
 
 export default function Home() {
   return (
     <div style={{ color: `purple`, fontSize: `72px` }}>
+      <Link to="/contact/">Contact</Link>
       <Header headerText={`Here's Gatsby`} />
-      <img src="https://source.unsplash.com/random/400x200" alt="random photo from unsplash" />
+      <img src="https://source.unsplash.com/random/400x200" alt="random pic from unsplash" />
     </div>
     )
 }
